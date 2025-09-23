6aab57fc04654e01560fda6e0259de6ba5346904
Nitin Bajaj <151855353+CSE-CY@users.noreply.github.com>
2025-09-23 19:17:35 +0530

    Create sum.cpp

---
+++ recursion/sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include<string.h>
+using namespace std;
+int sum(int n){
+if(n==0)return 0;
+    return n+sum(n-1);
+}
+
+int main(){
+  int n;
+  cout<<"Enter the number ";
+  cin>>n;
+  cout<<sum(n);
+
+  return 0;
+}
