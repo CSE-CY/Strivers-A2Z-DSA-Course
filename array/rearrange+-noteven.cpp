06d7ad7f1fe414071c0241ea86c8c271c129918a
Nitin Bajaj <151855353+CSE-CY@users.noreply.github.com>
2025-10-04 21:56:58 +0530

    Add files via upload

---
+++ array/rearrange+-noteven.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <math.h>
+#include<string.h>
+#include <string>
+#include<vector>
+using namespace std;
+void rearrange(vector<int> &arr,int n){
+  vector<int>pos,neg;
+  for(int i=0;i<n;i++){
+    if(arr[i]>0){
+      pos.push_back(arr[i]);
+    }
+    else{
+      neg.push_back(arr[i]);
+    }
+  }
+  if(pos.size()>neg.size()){
+  for(int i=0;i<neg.size();i++){
+    arr[2*i]=pos[i];
+    arr[2*i+1]=neg[i];
+  }
+  int index=neg.size()*2;
+  for(int i=neg.size();i<pos.size();i++){
+    arr[index]=pos[i];
+    index++; 
+  }}
+  else{ for(int i=0;i<pos.size();i++){
+    arr[2*i]=pos[i];
+    arr[2*i+1]=neg[i];
+  }
+  int index=pos.size()*2;
+  for(int i=pos.size();i<neg.size();i++){
+    arr[index]=neg[i];
+    index++; 
+  }
+  }
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+}
+int main(){
+int n;
+  cout<<" array size"<<endl;
+  cin>>n;
+  vector<int>arr(n);
+  cout<<"enter + and - value"<<endl;
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  rearrange(arr,n);
+  return 0;
+}
